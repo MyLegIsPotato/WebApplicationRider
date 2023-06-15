24e49604426fa52524ae5b1fdc9b07344fc89844
Lel Lelovsky <12matQ@gmail.com>
2023-06-15 13:03:20 +0200

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace WebApplicationRider.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
