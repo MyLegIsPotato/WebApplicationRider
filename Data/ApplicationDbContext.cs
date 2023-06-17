3eda50d7f776119e86222a19629b140d6ba756dd
Lel Lelovsky <12matQ@gmail.com>
2023-06-17 20:23:19 +0200

    Before "migration" commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplicationRider.Models;
+
+namespace WebApplicationRider.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+    }
+}
